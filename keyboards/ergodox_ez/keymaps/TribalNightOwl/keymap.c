91ccfc561271864b27ffbe34c54ea7ce2feb591e
Hugo Gayosso <tribrhy@gmail.com>
2017-12-24 18:37:24 -0500

    tap dances have their own file now

--- keyboards/ergodox_ez/keymaps/TribalNightOwl/keymap.c
+++ keyboards/ergodox_ez/keymaps/TribalNightOwl/keymap.c
@@ -2,72 +2,13 @@
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
+#include "tapdances.h"
 
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MOVE 2 // move keys
 
-// Enums defined for all examples:
-enum {
- CT_DOT_CLN= 0,
- CT_COMM_SCLN,
- CT_CLN,
- CT_EGG,
- CT_FLSH,
- X_TAP_DANCE,
- TD_ESC_CAPS
-};
-
-void dance_egg (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count >= 4) {
-    SEND_STRING ("Safety dance!");
-    reset_tap_dance (state);
-  }
-}
-
-
-void dance_dot_cln_finished (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code (KC_DOT);
-  } else {
-	register_code (KC_RSFT);
-    register_code (KC_SCLN);
-  }
-}
-
-void dance_dot_cln_reset (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code (KC_DOT);
-  } else {
-	unregister_code (KC_RSFT);
-    unregister_code (KC_SCLN);
-  }
-}
-
-void dance_comm_scln_finished (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code (KC_COMM);
-  } else {
-    register_code (KC_SCLN);
-  }
-}
-
-void dance_comm_scln_reset (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    unregister_code (KC_COMM);
-  } else {
-    unregister_code (KC_SCLN);
-  }
-}
-
-//All tap dance functions would go here. Only showing this one.
-qk_tap_dance_action_t tap_dance_actions[] = {
- [CT_DOT_CLN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_dot_cln_finished, dance_dot_cln_reset),
- [CT_COMM_SCLN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_comm_scln_finished, dance_comm_scln_reset),
- [CT_EGG] = ACTION_TAP_DANCE_FN (dance_egg),
- [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
-};
 
 
 enum custom_keycodes {
@@ -103,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
@@ -115,8 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   TD(CT_DOT_CLN),GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,  TD(CT_COMM_SCLN),TD(CT_DOT_CLN), CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_RSFT, KC_RCTL,KC_LBRC,KC_RBRC,          KC_FN1,
+             MEH_T(KC_NO),KC_N,   KC_M,  TD(CT_COMM_SCLN),TD(CT_DOT_CLN), TD(X_TAP_DANCE),   KC_RSFT,
+                                  KC_RSFT, KC_RCTL,KC_RALT,KC_RBRC,          KC_FN1,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
@@ -172,24 +113,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      | Left | Down | Right|      |  Play  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |VolUp |VolDn | Mute |      |      |
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 | Lclk | Rclk |------|       |------|      |Back  |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 | Lclk | Rclk |------|       |------|      |      |
  *                                 |      |      | Mclk |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // MEDIA AND MOUSE
 [MOVE] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_WH_U, KC_TRNS, TO(BASE),
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_WH_U, TO(BASE),
        KC_TRNS, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WH_D, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WH_D, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
@@ -198,11 +139,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 	   TO(BASE),  KC_TRNS, KC_PGUP, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_PGDN, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
 	   TD(TD_ESC_CAPS),
-       TD(CT_EGG), KC_TRNS, KC_WBAK
+       TD(CT_EGG), KC_TRNS, KC_TRNS
 ),
 };
 
